7a2f9534e54323c9c790398926bbcea0412277cd
vmquyetnb <vuminhquyetym@gmail.com>
2024-08-13 16:24:03 +0700

    call API send message

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -2,30 +2,53 @@
     <div class="sidebar p-3 w-100">
         <div class="d-flex flex-column gap-2 px-1">
             <div class="d-flex  justify-content-between align-items-center">
-                <i class="icon bi bi-window"></i>
-                <i class="icon bi bi-folder-plus"></i>
+                <i class="icon bi bi-window" @click="addConversation()" ></i>
+                <i class="icon bi bi-folder-plus" @click="addConversation()" ></i>
             </div>
-            <button class="new-chat w-100 px-2 d-flex gap-2">
+            <button class="new-chat w-100 px-2 d-flex gap-2" @click="addConversation()">
                 <img src="../assets/gpt.svg" alt="gpt-logo">
                 <span>BotGPT</span>
             </button>
             <div class="time w-100">
                 <span class="ms-1">Hôm nay</span>
-                <button class="btn-chat text-start mt-2 p-1 w-100">
-                    User hỏi 
-                </button>
+                <div v-for="conversation in listConversation">
+                    <button class="btn-chat text-start mt-2 p-1 w-100">
+                        {{ conversation.title }}
+                    </button>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script setup>
+import { ref, onMounted, onUnmounted } from 'vue';
+import axios from 'axios';
 
+const conversation = ref({
+    title : "New Chat"
+})
+
+const listConversation = ref([]);
+
+const getConversation = async () => {
+    const res = await axios.get('http://localhost:8080/conversation')
+    listConversation.value = res.data.output;
+    console.log(res.data.output);
+    return res;
+}
+
+const addConversation = async () => {
+    const res = await axios.post('http://localhost:8080/conversation/add',conversation.value);
+    getConversation();
+    return res;
+}
+getConversation();
 </script>
 
 <style scoped>
 .sidebar{
-    min-height: 934px;
+    min-height: 100vh;
     background: #000;
     color: #fff;
     font-size: 14px;
@@ -55,7 +78,7 @@
 }
 .btn-chat{
     font-weight: normal;
-    font-size: 14px;
+    font-size: 12px;
     background: #000;
     outline: none;
     border: none;
--- src/views/ChatWindow.vue
+++ src/views/ChatWindow.vue
@@ -6,21 +6,33 @@
                 <img src="../assets/gpt.svg" alt="gpt.svg">
             </div>
             <div class="d-flex justify-content-center w-100">
-                <div class="input w-100 d-flex flex-column gap-2">
-                    <div class="message d-flex flex-row-reverse">
-                        <div class="user-message">
-                            <p class="mb-0 text-break">aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+                <div class="input w-100 d-flex flex-column gap-2" >
+                    <div v-for="(msg,index) in messages" :key="index">
+                        <div class="message d-flex flex-row-reverse" v-if="msg.sender === 'USER'">
+                            <div class="user-message">
+                                <p class="mb-0 text-break">{{ msg.content }}</p>
+                            </div>
                         </div>
-                    </div>
-                    <div class="d-flex gap-2">
-                        <img  style="width: 24px;height: max-content;" src="../assets/gpt.svg" alt="">
-                        <div class="bot-message">
-                            <p class="mb-0 text-break">bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb</p>
+                        <div>
+                            <div class="d-flex gap-2" v-if="msg.sender === 'BOT'">
+                                <img  style="width: 24px;height: max-content;" src="../assets/gpt.svg" alt="">
+                                <div class="bot-message">
+                                    <p class="mb-0 text-break">{{ msg.content }}</p>
+                                </div>
+                            </div>
+                            <div class="d-flex gap-2" v-if="loading && index === messages.length - 1">
+                                <img  style="width: 24px;height: max-content;" src="../assets/gpt.svg" alt="">
+                                <div class="bot-message">
+                                    <div class="w-100">
+                                        <div class="loader"></div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div class="input-group w-100 flex-nowrap">
-                        <input type="text" class="form-control" placeholder="Tin nhắn BotGPT" aria-label="Username" aria-describedby="addon-wrapping">
-                        <span class="input-group-text" id="addon-wrapping"><i class="bi bi-send"></i></span>
+                        <input type="text" class="form-control" @keydown.enter="sendMessage()" v-model="message.content" placeholder="Tin nhắn BotGPT" aria-label="Username" aria-describedby="addon-wrapping">
+                        <span @click="sendMessage()" class="input-group-text" id="addon-wrapping"><i class="bi bi-send"></i></span>
                     </div>
                 </div>
             </div>
@@ -29,13 +41,75 @@
 </template>
 
 <script setup>
+import { ref, onMounted, onUnmounted } from 'vue';
+import axios from 'axios';
+import Stomp from 'stompjs';
+import SockJS from 'sockjs-client';
+
+const loading = ref(true);
+const messages = ref([]);
+const conversationId = ref(1)
+
+const message = ref({
+    content: "",
+});
+
+const stompClient = Stomp.over(new SockJS('http://localhost:8080/ws'));
+stompClient.debug = null;
+
+const sendMessage = async () => {
+  if (message.value.content.trim() === '') return;
+  try {
+    const res = await axios.post(
+        'http://localhost:8080/message/send',
+        { 
+            content: message.value.content 
+        },
+        {
+            headers: {
+            'Content-Type': 'application/json',
+            },
+            params: { 
+                conversationId: conversationId.value 
+            },
+            withCredentials: true,
+        });
+        loading.value = true;
+        message.value.content = "";
+        return res;
+    } catch (error) {
+        console.error('Error saving message:', error);
+    }
+};
+
+const onMessageReceived = (message) => {
+    const newMessage = JSON.parse(message.body);
+    messages.value.push(newMessage);
+    if (newMessage.sender === "BOT") {
+        loading.value = false;
+    }
+};
+
+onMounted(() => {
+    stompClient.connect({}, (frame) => {
+        stompClient.subscribe('/topic/messages', onMessageReceived);
+    }, (error) => {
+        console.error('Error connecting to WebSocket:', error);
+    });
+});
+
+onUnmounted(() => {
+    if (stompClient) {
+        stompClient.disconnect();
+    }
+});
 
 </script>
 
 <style scoped>
 .chat-window{
     background: #333;
-    min-height: 934px;
+    min-height: 100vh;
     color: #ffffff;
 }
 .header-text{
@@ -77,5 +151,17 @@ input::placeholder {
     background: #222222;
     width: max-content;
     border-radius: 24px;
+}
+.loader {
+  width: 5px;
+  aspect-ratio: 1;
+  border-radius: 50%;
+  animation: l5 1s infinite linear alternate;
+}
+@keyframes l5 {
+    0%  {box-shadow: 8px 0 #fff, -8px 0 #fff2;background: #fff }
+    33% {box-shadow: 8px 0 #fff, -8px 0 #fff2;background: #fff2}
+    66% {box-shadow: 8px 0 #fff2,-8px 0 #fff; background: #fff2}
+    100%{box-shadow: 8px 0 #fff2,-8px 0 #fff; background: #fff }
 }
 </style>
